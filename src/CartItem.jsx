b94d9b68d039b9a4064a7d65a6def5113b0033d7
Anna Poga <anna.poga988@gmail.com>
2025-10-01 05:57:07 -0400

    Implemented total amount

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -9,7 +9,15 @@ const CartItem = ({ onContinueShopping }) => {
 
   // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
- 
+    let total = 0;
+
+    cart.forEach(item => {
+        const cost = parseFloat(item.cost.substring(1));
+
+        total += cost * item.quantity;
+    });
+
+    return total;
   };
 
   const handleContinueShopping = (e) => {
